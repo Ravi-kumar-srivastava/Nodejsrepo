c77b7190f39dcdbf2a90af47df94d3a49c9b4a5e
Rkravikr12 <rkravikr12@gmail.com>
2023-04-27 17:57:07 +0530

    first commit aahey

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var express = require(‘express’);
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get(‘/’, function (req, res) {
- res.send(‘hello world’);
+ res.send(‘Hello Special Batch’);
 });//listen to port 80 by default
 app.listen(process.env.PORT || 80);
 module.exports = app;
